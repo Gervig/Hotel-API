e00f6eb1492f20c9a2a2c2be4c4aa9c297a30fce
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-03-12 09:57:41 +0100

    Påbegyndt Security layer

---
+++ src/main/java/app/security/entities/Role.java
@@ -0,0 +1,23 @@
+package app.security.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    String name;
+    @ManyToMany(mappedBy = "roles")
+    Set<User> users = new HashSet<>();
+
+    public Role(String name){
+        this.name = name;
+    }
+    public Role(){}
+}
